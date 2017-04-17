e2109eff3ae67327d4c079164e8c9ca35ba803b3
SimmaLimma <simonmaartensson@gmail.com>
2017-04-17 14:18:06 +0200

    Update spi_buffer.h

--- spi_buffer.h
+++ spi_buffer.h
@@ -5,6 +5,10 @@
 
 #define SPI_BUFFER_SIZE 64 
 
+volatile extern unsigned char spi_rx_not_empty_flag;
+
+void spi_master_init(void);
+
 unsigned char spi_get_byte(void);
 
 void spi_send_byte(unsigned char value);
