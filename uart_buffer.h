77c77704cdc571564be3ed3392e5796e70012177
SimmaLimma <simonmaartensson@gmail.com>
2017-05-12 11:57:44 +0200

    Update uart_buffer.h

--- uart_buffer.h
+++ uart_buffer.h
@@ -14,12 +14,15 @@
 #define BUFFER_SIZE 64 
 
 volatile extern unsigned char uart0_rx_not_empty_flag;
-volatile extern unsigned char uart0_tx_not_empty_flag;
-volatile extern unsigned char uart0_rx_overf_flag;
-volatile extern unsigned char uart0_tx_overf_flag;
-volatile extern unsigned char uart0_rx_full_flag;
-volatile extern unsigned char uart0_tx_full_flag;
+//volatile extern unsigned char uart0_tx_not_empty_flag;
+//volatile extern unsigned char uart0_rx_overf_flag;
+//volatile extern unsigned char uart0_tx_overf_flag;
+//volatile extern unsigned char uart0_rx_full_flag;
+//volatile extern unsigned char uart0_tx_full_flag;
 
+
+//init
+void USART0_init(unsigned int baud_setting);
 
 //uart0_send_byte is a function to place a byte in Tx_buffer
 void uart0_send_byte(unsigned char value);
@@ -27,5 +30,7 @@ void uart0_send_byte(unsigned char value);
 //uart0_get_byte is a function to retrieve a byte from Rx_buffer, which is returned
 unsigned char uart0_get_byte(void);
 
+//unsigned char read_comp_info();
+unsigned char read_comp_info(unsigned char* control_com, unsigned char* nod_info_ptr, unsigned char *manual_mode_ptr, unsigned char *dest_list_ptr);
 
 #endif
