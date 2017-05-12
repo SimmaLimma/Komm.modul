b0bbaacb571a03fd7b5e286b5f416cfd3bb3b956
SimmaLimma <simonmaartensson@gmail.com>
2017-05-12 11:56:21 +0200

    Update spi_buffer.h

--- spi_buffer.h
+++ spi_buffer.h
@@ -4,6 +4,7 @@
 #define SPI_INCL_GUARD_BUFFER
 
 #define SPI_BUFFER_SIZE 64 
+#define RECEIVED_SPI_PACKAGE_SIZE 4
 
 volatile extern unsigned char spi_rx_not_empty_flag;
 
@@ -13,6 +14,8 @@ unsigned char spi_get_byte(void);
 
 void spi_send_byte(unsigned char value);
 
+unsigned char is_spi_package_received(void);
 
+unsigned char read_steering_info(unsigned char* esc_valueL_ptr,unsigned char* esc_valueH_ptr, unsigned char* steering_valueL_ptr, unsigned char* steering_valueH_ptr);
 
 #endif
